76e00f135b420898f1bf44ec64ed914b9b92de6d
tatperry <66287436+tatperry@users.noreply.github.com>
2021-02-11 16:42:30 -0500

    changed lab 2

--- n220_Spring2021/Lab03/loopsExercises/fizzBuzz/fizz.js
+++ n220_Spring2021/Lab03/loopsExercises/fizzBuzz/fizz.js
@@ -5,18 +5,42 @@
 // FEBRUARY 11, 2021
 
 // variable 1-100 of numbers 
-let start = 1,
-    end = 100,
-    i;
-// letting it go to 100 
-for (i = start; i <= end; i++) {
-  let str = '';
-  // anything divided by 3 is fizz
-  if (i % 3 == 0) str += 'Fizz';
-  // anything divided by 5 is buzz
-  if (i % 5 == 0) str += 'Buzz';
-  // anything divided by both 3 and 5 is both words
-  if (str.length === 0) str = i;
-  // running the code
-  console.log(str);
+// MAKE A CANVAS AND SET A BACKGROUND COLOR
+function setup() {
+  createCanvas(900, 100);
+  background(205);
+}
+// FIZZ BUZZ/ MAKINGING CIRCLES AND SQUARES
+function draw() {
+  // IF STATEMENT
+
+  for (var i = 1; i < 26; i++) {
+    // if the items are divisible by 5 and 3 make them a BLUE RECTANGLES
+
+    if (i % 3 == 0 && i % 5 == 0) {
+      fill("blue");
+      rect(i * 30, 15, 10, 10);
+      // (x,y,width,height);
+    }
+
+    // if the items are divisible by 5 make the GREEN RECTANGLES
+    else if (i % 5 == 0) {
+      fill("green");
+      rect(i * 30, 15, 10, 10);
+
+      // (x,y,width,height);
+    }
+
+    // if the items are divisible by 3 make them PURPLE CIRCLES
+    else if (i % 3 == 0) {
+      fill("purple");
+      circle(i * 30, 20, 10);
+    }
+
+    // All the rest should be black
+    else {
+      fill("black");
+      circle(i * 30, 20, 10);
+    }
+  }
 }
