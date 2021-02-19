a3791ac0962612b771bae43940ed5985470cec3a
tatperry <66287436+tatperry@users.noreply.github.com>
2021-02-18 22:23:20 -0500

    Updated lab 4 worked on snakes portion

--- n220_Spring2021/Lab04/snake/js/app.js
+++ n220_Spring2021/Lab04/snake/js/app.js
@@ -3,4 +3,20 @@
 // Lab04
 // February 18, 2021
 
-let cirlce = [];
+let cirlce = [0,0,0,0,0,0,0,0,0,0];
+function setup(){
+    createCanvas(400,300);
+    
+}
+
+function draw(){
+    console.log(cirlce);
+    background(10, .9);
+
+    cirlce.push( mouseX);
+    cirlce.shift();
+
+    for(var i=0; i< cirlce.length; i++ ){
+        circle(cirlce[i], 150, 30);
+    }
+}
