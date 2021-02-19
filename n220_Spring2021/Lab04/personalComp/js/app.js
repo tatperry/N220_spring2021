c8e90e23460fee8c40d9cc08c653bcd556902d20
tatperry <66287436+tatperry@users.noreply.github.com>
2021-02-18 23:24:46 -0500

    added two new rectangles to the personal composition lab

--- n220_Spring2021/Lab04/personalComp/js/app.js
+++ n220_Spring2021/Lab04/personalComp/js/app.js
@@ -4,19 +4,39 @@
 // February 18, 2021
 
 let comp = [0,0,0,0];
+
+let square = [0,0,0,0];
+
+let rectangle =[0,0,0,0];
+
 function setup(){
-    createCanvas(400,300);
+    createCanvas(400,400);
     
 }
 
 function draw(){
     console.log(comp);
-    background(10);
-    comp.push();
-    comp.shift(mouseX);
-    for(var i=0; i< comp.length; i++ ){
-        circle(comp[i], 150, 30);
-        rect(comp[i], 150, 30);
-        circle(comp[i], 150, 30);
+    console.log(square);
+    console.log(rectangle);
+    background(10, .9);
+    comp.push(mouseX);
+    square.push(mouseX);
+    rectangle.push(mouseX);
+
+// top squares
+    for(var i=0; i< rectangle.length; i++){
+        fill(900,0,0,0);
+        rect(rectangle[i], 70, 30);
     }
+    // middle squares
+    for(var i=0; i< comp.length; i++ ){
+        fill(30, 0, 0, 0);
+        rect(comp[i], 170, 30);   
+    }
+    // bottom squares
+    for(var i=0; i< square.length; i++){
+        fill(900,0,0,0);
+        rect(square[i], 270, 30);
+    }
+
 }
