c0f0b718ec6059481b9563ceafc6ea926b66932f
tatperry <66287436+tatperry@users.noreply.github.com>
2021-02-18 22:43:10 -0500

    changed dripping rain to personal comp

---
+++ n220_Spring2021/Lab04/personalComp/js/app.js
@@ -0,0 +1,20 @@
+// Tatiana Perry
+// NEWM-N220
+// Lab04
+// February 18, 2021
+
+let cirlce = [0,0,0,0,0,0,0,0,0,0];
+function setup(){
+    createCanvas(400,300);
+    
+}
+
+function draw(){
+    console.log(cirlce);
+    background(10, .9);
+    cirlce.push( mouseX);
+    cirlce.shift();
+    for(var i=0; i< cirlce.length; i++ ){
+        circle(cirlce[i], 150, 30);
+    }
+}
